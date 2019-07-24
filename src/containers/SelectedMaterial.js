29d0bbbfaab48b6eb58e166cefd3a22aadc16ca5
kirstenwerner <kirstenmwerner@gmail.com>
2019-07-24 09:00:36 -0700

    WIP selectedMaterial container

---
+++ src/containers/SelectedMaterial.js
@@ -0,0 +1,18 @@
+import React from "react"
+import MaterialCard from "../components/materials/MaterialCard"
+
+class SelectedMaterial extends React.Component {
+
+  showMaterial = () => {
+    return this.props.selectedMaterial
+      return <MaterialCard material={material} name={material.name} description={material.description} key={material.id}/>
+  }
+
+  render(){
+    return(
+      <div className="selected material">
+        {this.showMaterial}
+      </div>
+    )
+  }
+}
