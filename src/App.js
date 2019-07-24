f206b62b15459f14cf98fbcbe33ff5c57981ab70
kirstenwerner <kirstenmwerner@gmail.com>
2019-07-24 08:55:16 -0700

    set initial app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {connect} from 'react-redux'
+import {fetchMaterials} from './actions/materialsActions'
+import MaterialsContainer from './containers/MaterialsContainer';
+import RecycleCentersContainer from './containers/RecycleCentersContainer'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MaterialsContainer />
+      <RecycleCentersContainer />
     </div>
   );
 }
