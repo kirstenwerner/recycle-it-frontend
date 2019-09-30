ce131b9c1a7f90a292041718e7eaebd1bce1d307
kirstenwerner <kirstenmwerner@gmail.com>
2019-09-30 13:39:43 -0700

    update .gitignore

--- src/reducers/rootReducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux"
-import manageMaterials from "./manageMaterials"
-import manageRecycleCenters from "./manageRecycleCenters"
-
-const rootReducer = combineReducers({materials: manageMaterials, recycleCenters: manageRecycleCenters})
-
-export default rootReducer
