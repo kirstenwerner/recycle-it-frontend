5e186b06606ea85d66547d9fc15d7d1d4a33f96b
kirstenwerner <kirstenmwerner@gmail.com>
2019-11-04 14:18:35 -0800

    attempting to get API key to work with heroku

--- src/containers/RecycleCentersMapContainer.js
+++ src/containers/RecycleCentersMapContainer.js
@@ -10,7 +10,7 @@ export default class RecycleCenterssMapContainer extends React.Component {
 			<div className="map-container">
 			<Map
 				recycleCenters={this.props.recycleCenters}
-				googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${KEY}&v=3.exp&libraries=geometry,drawing,places`}
+				googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.MAP_API_KEY}&v=3.exp&libraries=geometry,drawing,places`}
 				loadingElement={<div style={{ height: `100%` }} />}
 				containerElement={<div style={{ height: `600px`, width: `100%` }} />}
 				mapElement={<div style={{ height: `100%` }} />}
